ac6c27dc848a05b2ae89e2b9213fd74d699da21a
Daniel Avzaradel <daniela@cgen.com>
2022-04-10 15:27:48 +0300

    finished second chart

--- src/components/Covid2.js
+++ src/components/Covid2.js
@@ -19,17 +19,29 @@ function Covid2(props) {
               type: 'bar',
               mode: 'markers',
               marker: {
-                color: '#0066ff',
+                color: 'orange',
               },
-              name: 'Test',
+              name: 'Total cases',
+            },
+            {
+              //   x: [1, 2, 3],
+              //   y: [1, 2, 3],
+              x: covidData.data.map((item) => item.date),
+              y: covidData.data.map((item) => item.testing.total.value),
+              type: 'bar',
+              mode: 'markers',
+              marker: {
+                color: 'blue',
+              },
+              name: 'Testing',
             },
           ]}
           layout={{
-            title: '',
+            title: 'Number of cases',
             yaxis: {
               title: 'No. of Cases',
             },
-            plot_bgcolor: '#e2e2e2',
+            plot_bgcolor: '#f2f2f2',
             xaxis: {
               title: 'Date',
             },
